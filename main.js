69694bc859c24fb69f6bdbbf78a4e7c7bc50b35f
alebaviera <baviera.alessandra@gmailcom>
2019-07-17 16:26:10 +0200

    counter

--- main.js
+++ main.js
@@ -3,18 +3,37 @@ $(document).ready(function(){
 
 
 console.log('ciao griglia');
-
+var elementogreen = document.getElementById('countgreen');
+var elementored = document.getElementById('countred');
+var countgreen = 0;
+var countred = 0;
 $('.quadrato:not(.qui)').click(
+
 function (){
+
   console.log('click');
-  $(this).addClass('green');
+  if($(this).addClass('green')){
+    countgreen++;
+    console.log(countgreen);
+    // countgreen.addClass('greentext');
+    elementogreen.innerHTML = countgreen;
+  }
+
+
 });
 
-
-
 $('.qui').click(function (){
+
   console.log('click');
-  $(this).addClass('red');
+  if($(this).addClass('red')){
+    countred++;
+    console.log(countgreen);
+    // countred.addClass('redtext');
+      elementored.innerHTML = countred;
+
+  }
+
+
 });
 
 
